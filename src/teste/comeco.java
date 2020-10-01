265f9b2af609665c9b20ff578aea7dba8324c119
jeffersonlk <jefferson_kl@outlook.com>
2020-09-30 22:01:55 -0300

    primeiro teste

---
+++ src/teste/comeco.java
@@ -0,0 +1,16 @@
+package teste;
+import java.util.Scanner;
+import java.util.Date;
+
+public class comeco {
+
+	public static void main(String[] args) {
+		
+	  Date a = new Date ();
+	  
+	  System.out.println(a);
+		
+	}
+
+}
+
